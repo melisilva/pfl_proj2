b59d32966ec9e3481f56ffb62a68b41e601356e5
Mateus Silva <42583814+lessthelonely@users.noreply.github.com>
2021-12-20 20:06:23 +0000

    we need the tests for white, black and empty somehow

--- board.pl
+++ board.pl
@@ -1,6 +1,10 @@
 :- use_module(library(lists)).
 :- consult('./utils.pl').
 
+isEmpty(X) :- X = 'empty'.
+isBlack(X) :- X = 'black'.
+isWhite(X) :- X = 'white'.
+
 initialBoard([
     [black,black,black,black,black,black,black,black,black],
     [black,black,black,black,black,black,black,black,black],
@@ -13,9 +17,6 @@ initialBoard([
     [white,white,white,white,white,white,white,white,white]
 ]).
 
-direction(ver).
-direction(hor).
-
 play(R, C, H, V) :-
     initialBoard(X),
     nth0(R, X, Line),
@@ -24,14 +25,12 @@ play(R, C, H, V) :-
     nth0(I1, X, Line1),
     I2 is C + V,
     nth0(I2, Line1, Col1),
-    (Col1 is empty
-    -> (Col is white
+    (isEmpty(Col1) 
+    -> (isWhite(Col)
        -> I is C + V, delete_elem(I, Line1, E, Line2), nth0(I, Line2, white, Line1)
        ; I is C + V, delete_elem(I, Line1, E, Line2), nth0(I, Line2, black, Line1)
        )
-    ; error('Não foi escolhida uma posição-alvo vazia.')
-    ),
-    Col1 is Col,
-    Col is empty.
+    ; error('Posicao alvo preenchida.')
+    ).
 
 error(X) :- print(X).
