74bd1c7025ab6d13ffbe805c5139d33121dacf86
Melissa Silva <melissa.silva2001.ms@gmail.com>
2021-12-20 19:04:06 +0000

    Fixes

--- board.pl
+++ board.pl
@@ -18,20 +18,20 @@ direction(hor).
 
 play(R, C, H, V) :-
     initialBoard(X),
-    nth0(R, X, FChar),
-    nth0(C, FChar, RChar),
+    nth0(R, X, Line),
+    nth0(C, Line, Col),
     I1 is R + H,
-    nth0(I1, X, line1),
+    nth0(I1, X, Line1),
     I2 is C + V,
-    nth0(I2, line1, col1),
-    (col1 is empty
-    -> (col is white
-       -> I is C + V, delete_elem(I, line1, E, line2), nth0(I, line2, white, line1)
-       ; I is C + V, delete_elem(I, line1, E, line2), nth0(I, line2, black, line1)
+    nth0(I2, Line1, Col1),
+    (Col1 is empty
+    -> (Col is white
+       -> I is C + V, delete_elem(I, Line1, E, Line2), nth0(I, Line2, white, Line1)
+       ; I is C + V, delete_elem(I, Line1, E, Line2), nth0(I, Line2, black, Line1)
        )
     ; error('Não foi escolhida uma posição-alvo vazia.')
     ),
-    col1 is col,
-    col is empty.
+    Col1 is Col,
+    Col is empty.
 
 error(X) :- print(X).
