70c5b60b29fdd2e7ce946364d6277d4a37bc5a05
Melissa Silva <melissa.silva2001.ms@gmail.com>
2021-12-21 16:10:41 +0000

    Input working with repeat

--- input.pl
+++ input.pl
@@ -1,16 +1,22 @@
 isValidCoord(X) :- X =< 8, X >= 0.
 check_horizontal_and_vertical(H,V):- ((H==2; H== (-2)),(V==1; V== (-1))); ((H==1; H== (-1)),(V==2; V== (-2))).
         
+askforH_and_V(H,V):- print('Please input a value to add to row (horizontal): '),
+                     read(H),
+                     print('Please input a value to add to column (vertical): '),
+                     read(V),
+                     (\+check_horizontal_and_vertical(H,V)->askforH_and_V(H1,V1)).
+
 askForInput(R, C, V, H) :-
     print('Please input a Row (horizontal): '),
     read(R),
     print('Please input a Column (vertical): '),
     read(C),
     print('Please input a value to add to row (horizontal): '),
-    read(V),
-    print('Please input a value to add to collumn (vertical): '),
     read(H),
-    check_horizontal_and_vertical(H,V).
+    print('Please input a value to add to column (vertical): '),
+    read(V),
+    (\+check_horizontal_and_vertical(H,V)->askforH_and_V(H1,V1)); print('Well Done :)').
 
 
 
