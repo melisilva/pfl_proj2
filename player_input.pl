4cbbc42caa120b11714b2b7b99dc34d0621c25cb
Melissa Silva <melissa.silva2001.ms@gmail.com>
2021-12-21 16:28:57 +0000

    Delete player_input.pl

--- player_input.pl
+++
@@ -1,6 +0,0 @@
-move(GameState, Move, NewGameState):- selectPiece(GameState, Move, NewGameState, MoveWay, Piece),
-                                      movePiece(GameState, NewGameState, Move, MoveWay, Piece).
-
-selectPiece(GameState, Move, NewGameState, MoveWay, Piece):- getInput(GameState, SelectColumn, SelectRow, MoveWay),
-                                                             validateChoices(SelectColumn, SelectRow, GameState, NewGameState, Move, MoveWay, Piece).
-                                                             
