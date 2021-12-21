cfc346b60c4d1f8c1124154e46f77cb5ba10b624
Mateus Silva <42583814+lessthelonely@users.noreply.github.com>
2021-12-21 12:49:14 +0000

    oof

--- board.pl
+++ board.pl
@@ -5,6 +5,7 @@
 isEmpty(X) :- X == 0.
 isBlack(X) :- X == 1.
 isWhite(X) :- X == -1.
+isEqual(X, Y) :- X == Y.
 
 initialBoard([
     [1,1,1,1,1,1,1,1,1],
@@ -18,21 +19,38 @@ initialBoard([
     [-1,-1,-1,-1,-1,-1,-1,-1,-1]
 ]).
 
-play(R, C, H, V, X1) :-
+validPos('').
+isValidPos(R, C, V, H) :-
+    H1 is C + H,
+    V1 is R + V,
+    (R =< 8, R >= 0, C =< 8, C >= 0
+    -> (H1 =< 8, H1 >= 0, V1 =< 8, V1 >= 0
+        -> validPos('')
+        ; error('Jogada irrealizavel.')
+       )
+    ; error('Nao foi dada uma posicao correta.')
+    ).
+
+
+play(R, C, V, H, X1) :-
+    isValidPos(R, C, V, H),
     initialBoard(X),
     printBoard(X),
     nth0(R, X, Line),
     nth0(C, Line, Col),
-    I1 is R + H,
+    I1 is R + V,
     nth0(I1, X, Line1),
-    I2 is C + V,
+    I2 is C + H,
     nth0(I2, Line1, Col1),
     (isEmpty(Col1)  
     -> (isWhite(Col)
-       -> I is C + V, replace(I, Line1, -1, Line2), replace(R, X, Line2, X1)
-       ; I is C + V, replace(I, Line1, 1, Line2), replace(R, X, Line2, X1)
+       -> I is C + V, replace(I, Line1, -1, Line2), replace(R, X, Line2, X2), replace(C, Line, 0, Line3), replace(R, X2, Line3, X1)
+       ; I is C + V, replace(I, Line1, 1, Line2), replace(R, X, Line2, X2),  replace(C, Line, 0, Line3), replace(R, X2, Line3, X1)
        )
-    ; error('Posicao alvo nao esta vazia.')
+    ; (isEqual(Col1, Col)
+      -> error('Not a valid play!')
+      ; error('Play again.')
+      )
     ),
     printBoard(X1).
 
