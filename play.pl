f7fa96a4e6ae3c9846cc16a52c2ab92cab526afd
Melissa Silva <melissa.silva2001.ms@gmail.com>
2022-01-10 18:44:57 +0000

    Update play.pl

--- play.pl
+++ play.pl
@@ -16,32 +16,32 @@ initialBoard([
 
 changePlayer(P, P1, P1, P2).
 changePlayer(P, P2, P1, P2).
-changePlayer(P, NP, P1, P2) :-
+changePlayer(P, CP, P1, P2) :-
     (P = P1
     -> changePlayer(P, P2, P1, P2)
     ; changePlayer(P, P1, P1, P2)
     ).
 
 loop(-1, _).
-loop(I, X, CP, P1, P2, GameState) :-
-    askForInput(R, C, V, H, X, 'P1'),
+loop(I, X, CP, P1, P2) :-
+    askForInput(R, C, V, H, X, CP),
     print('R: '), print(R), nl,
     print('C: '), print(C), nl,
     print('V: '), print(V), nl,
     print('H: '), print(H), nl,
     print('X: '), print(X), nl,
-    play(R, C, V, H, X, X1,'P1'),
+    play(R, C, V, H, X, X1,CP),
     (check_WhitePlayer_won(P1) ; check_BlackPlayer_Won(P2)
     -> loop(-1, X1, P1, P2)
-    ; changePlayer(P, NP, P1, P2), loop(0, X1, NP, P1, P2)
-    ),loop(I,X,CP,P1,P2,GameState).
+    ; changePlayer(P, CP, P1, P2), loop(0, X1, CP, P1, P2)
+    ),loop(I,X,CP,P1,P2).
 
 start :-
     initialBoard(X),
     getPlayer1(P1),
     getPlayer2(P2),
     printBoard(X),
-    loop(0, X, P1, P1, P2,'start').
+    loop(0, X, P1, P1, P2).
 
 /*
 display_game(GameState)
